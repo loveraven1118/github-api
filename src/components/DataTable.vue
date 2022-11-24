67b86abf07f29e2324c20196c0bff21946c1ea6e
raven.su <raven422su@gmail.com>
2022-11-24 17:19:20 +0800

    create components DataTable.vue register components in IssueView

---
+++ src/components/DataTable.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="table">
+    <p>
+        Show me the issue list
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataTable'
+}
+</script>
--- src/views/IssueView.vue
+++ src/views/IssueView.vue
@@ -1,5 +1,17 @@
 <template>
     <div class="issue">
       <h1>Github Issue List</h1>
+      <DataTable />
     </div>
 </template>
+
+<script>
+import DataTable from '@/components/DataTable.vue'
+
+export default {
+  name: 'IssueView',
+  components: {
+    DataTable
+  }
+}
+</script>
