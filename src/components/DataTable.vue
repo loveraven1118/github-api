c1fc5b91b24e051fc84127fbcb9cffd9e03dbbec
raven.su <raven422su@gmail.com>
2022-11-25 16:14:12 +0800

    Get Issue Data

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,13 +1,58 @@
 <template>
   <div class="table">
-    <p>
-        Show me the issue list
-    </p>
+    <button @click="getdatas(1)">click</button>
+    <table>
+      <thead>
+        <tr>
+          <th>Issue Number</th>
+          <th>Issue Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="content in theadContent" :key="content.number">
+          <td>{{ content.number }}</td>
+          <td>{{ content.title }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+// API get issue list
+const pagesize = 10
+const username = 'loveraven1118'
+const repos = 'github-api'
+const headers = new Headers()
+headers.append('Accept', 'application/vnd.github.v3+json')
+const config = {
+  method: 'GET',
+  headers: headers,
+  redirect: 'follow'
+}
+
 export default {
-  name: 'DataTable'
+  name: 'DataTable',
+  data () {
+    return {
+      theadContent: []
+    }
+  },
+  methods: {
+    getdatas: function (page) {
+      const url = 'https://api.github.com/repos/' + username + '/' + repos + '/issues?per_page=' + pagesize + '&page=' + page
+
+      fetch(url, config).then(response => response.json())
+        .then(result => {
+          for (var i = 0; i < result.length; i++) {
+            this.theadContent.push({
+              number: result[i].number,
+              title: result[i].title
+            })
+          }
+        })
+        .catch(error => console.log('error', error))
+    }
+  }
 }
 </script>
